060cf503df2cb4a8c4ef49837226d1feaad62337
Yimu5781 <yimu19980412@hotmail.com>
2024-05-11 10:45:07 +0800

    Add: New class Database.

--- university/UniversityDatabase.java
+++ university/UniversityDatabase.java
@@ -20,31 +20,8 @@ public class UniversityDatabase {
         return new ArrayList<>(people);
     }
 
-    @Override
     public String toString() {
         return "UniversityDatabase{" + "people=" + people + '}';
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 29 * hash + Objects.hashCode(this.people);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final UniversityDatabase other = (UniversityDatabase) obj;
-        return Objects.equals(this.people, other.people);
     }
 
     public boolean add(Person p) {
